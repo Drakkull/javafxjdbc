a881e6bfdfb1c86d283a8be4a08c210ae5898583
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-06-22 09:45:43 -0300

    Added sql connector

--- src/main/java/orsegups/com/br/javafxjdbc/HelloController.java
+++ src/main/java/orsegups/com/br/javafxjdbc/HelloController.java
@@ -2,6 +2,7 @@ package orsegups.com.br.javafxjdbc;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+//possivelmente será excluido depois
 
 public class HelloController {
     @FXML
@@ -9,6 +10,7 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
+
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
